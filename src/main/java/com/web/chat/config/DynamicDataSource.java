e8ee63e539157a8fe862a10a3436be7840af9bb6
luochao <1152597708@qq.com>
2019-09-04 11:41:35 +0800

    修改test

--- src/main/java/com/web/chat/config/DynamicDataSource.java
+++ src/main/java/com/web/chat/config/DynamicDataSource.java
@@ -8,7 +8,7 @@ public class DynamicDataSource extends AbstractRoutingDataSource {
     public DynamicDataSource(DataSource defaultTargetDataSource, Map<Object, Object> targetDataSources) {
         super.setDefaultTargetDataSource(defaultTargetDataSource);
         super.setTargetDataSources(targetDataSources);
-        // afterPropertiesSet()方法调用时用来将targetDataSources的属性写入resolvedDataSources中的
+        // afterPropertiesSet()方法调用时用来将targetDataSources的属性写入resolvedDataSources中的,
         super.afterPropertiesSet();
     }
 
